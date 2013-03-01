7ff97ccc69b466f3f2b308588ccff64d97003a6e
Erik Schierboom <erik_schierboom@hotmail.com>
2013-03-01 11:54:55 +0100

    Implemented fitness function

---
+++ src/Domain/HelloGeneticAlgoritm.fs
@@ -0,0 +1,15 @@
+﻿namespace Domain
+
+open System
+
+module HelloGeneticAlgorithm =
+
+    let random = new Random()
+
+    let numericCharacterValues (str:string) =
+        List.map (fun c -> Convert.ToUInt16(c:char)) (List.ofArray (str.ToCharArray()))
+
+    let fitness (individual:string) =        
+        let zipped = List.zip (numericCharacterValues individual) (numericCharacterValues "hello world")
+        1.0 - List.fold (fun acc (x:uint16,y:uint16) -> acc + (Math.Abs(float x - float y) / float Char.MaxValue)) 0.0 zipped 
+            
---
+++ tests/Domain.Tests/HelloGeneticAlgoritmTests.fs
@@ -0,0 +1,27 @@
+﻿namespace Domain.Tests
+
+open Domain.HelloGeneticAlgorithm
+open System
+open Xunit
+
+type HelloGeneticAlgorithmTests() = 
+
+    [<Fact>]  
+    member this.numericCharacterValuesReturnsCorrectCharacterValues() =
+        Assert.True([uint16 104; uint16 101; uint16 108; uint16 108; uint16 111] = numericCharacterValues "hello")
+
+    [<Fact>]  
+    member this.fitnessOnCorrectlyCalculatesFitness() =
+        Assert.Equal(1.0, fitness "hello world")
+
+    [<Fact>]  
+    member this.fitnessOfMoreCorrectIndividualIsHigherThanLessCorrectIndividual() =
+        Assert.True(fitness "halla world" > fitness "xalla porla")
+
+    [<Fact>]  
+    member this.fitnessOnIndividualWithLengthLessThanTargetStringThrowsArgumentException() =
+        Assert.Throws<ArgumentException>(fun() -> fitness "hello worl" |> ignore)
+
+    [<Fact>]  
+    member this.fitnessOnIndividualWithLengthGreaterThanTargetStringThrowsArgumentException() =
+        Assert.Throws<ArgumentException>(fun() -> fitness "hello worlds" |> ignore)
