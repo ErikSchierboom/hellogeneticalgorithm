d46173b26cf192233a3189f151efc592cbd1eaac
Erik Schierboom <erik_schierboom@hotmail.com>
2013-03-01 13:44:29 +0100

    Added support for generating a population and mutating strings

--- src/Domain/StringGeneticAlgoritm.fs
+++ src/Domain/StringGeneticAlgoritm.fs
@@ -6,13 +6,40 @@ module StringGeneticAlgorithm =
 
     let random = new Random()
 
+    let numericCharacterValues (str:string) =
+        if str = null then raise (ArgumentNullException("str"))
+        List.map (fun c -> Convert.ToUInt16(c:char)) (List.ofArray (str.ToCharArray()))
+
     let generateRandomIndividual length =
         List.fold (fun acc i -> acc + Convert.ToChar(random.Next(128)).ToString()) "" [1..length]
 
-    let numericCharacterValues (str:string) =
-        List.map (fun c -> Convert.ToUInt16(c:char)) (List.ofArray (str.ToCharArray()))
+    let generatePopulation size length =
+        List.init size (fun i -> generateRandomIndividual length)
 
-    let fitness (individual:string) (target:string) =        
+    let mutateCharacter (c:char) =
+        if c > char 127 then raise (ArgumentException("The character must be an ASCII character (range [0..127])"))
+        match c with
+        | '\000' -> '\001' 
+        | '\127' -> '~' 
+        | _      -> if random.Next(2) = 1 then char (Convert.ToUInt16(c) + uint16 1) else char (Convert.ToUInt16(c) - uint16 1)            
+
+    let mutateCharacterAtIndex index (str:string) =
+        if str = null then raise (ArgumentNullException("str"))
+        if str.Length = 0 then raise (ArgumentException("The string to mutate must not have length zero."))
+        if index < 0 || index >= str.Length then raise (ArgumentException("The index must be greater than or equal to zero and be less than the length of the string."))
+        let chars = str.ToCharArray()
+        chars.[index] <- mutateCharacter chars.[index]
+        new string(chars)
+
+    let mutate (probability:float) (str:string) =
+        if str = null then raise (ArgumentNullException("str"))
+        if str.Length = 0 then raise (ArgumentException("The string to mutate must not have length zero."))
+        if float (random.Next(101)) > probability * 100.0 then str 
+        else mutateCharacterAtIndex (random.Next(str.Length)) str            
+
+    let fitness (target:string) (individual:string) =       
+        if target = null then raise (ArgumentNullException("individual"))
+        if individual = null then raise (ArgumentNullException("individual"))        
         let zipped = List.zip (numericCharacterValues individual) (numericCharacterValues target)
         1.0 - List.fold (fun acc (x:uint16,y:uint16) -> acc + (Math.Abs(float x - float y) / float Char.MaxValue)) 0.0 zipped 
             
--- tests/Domain.Tests/StringGeneticAlgoritmTests.fs
+++ tests/Domain.Tests/StringGeneticAlgoritmTests.fs
@@ -3,8 +3,17 @@
 open Domain.StringGeneticAlgorithm
 open System
 open Xunit
+open Xunit.Extensions
 
 type StringGeneticAlgorithmTests() = 
+
+    [<Fact>]  
+    member this.numericCharacterValuesReturnsCorrectCharacterValues() =
+        Assert.True([uint16 104; uint16 101; uint16 108; uint16 108; uint16 111] = numericCharacterValues "hello")
+
+    [<Fact>]  
+    member this.numericCharacterValuesWithNullStringThrowsArgumentNullException() =
+        Assert.Throws<ArgumentNullException>(fun() -> numericCharacterValues null |> ignore)
 
     [<Fact>]  
     member this.generateRandomIndividualGeneratesRandomIndividualOfSpecifiedLength() =
@@ -16,8 +25,72 @@ type StringGeneticAlgorithmTests() =
         Assert.False(generateRandomIndividual 10 = generateRandomIndividual 10)
 
     [<Fact>]  
-    member this.numericCharacterValuesReturnsCorrectCharacterValues() =
-        Assert.True([uint16 104; uint16 101; uint16 108; uint16 108; uint16 111] = numericCharacterValues "hello")
+    member this.generatePopulationGeneratesPopulationOfRandomIndividuals() =              
+        Assert.Equal<int>(100, generatePopulation 100 5 |> Seq.distinct |> Seq.length)
+
+    [<Fact>]  
+    member this.generatePopulationGeneratesPopulationOfRandomIndividualsOfSpecifiedSize() =      
+        Assert.Equal<int>(100, generatePopulation 100 5 |> List.length)
+
+    [<Fact>]  
+    member this.generatePopulationGeneratesPopulationOfRandomIndividualsWithSpecifiedLength() =              
+        Assert.True(List.forall (fun (elem:string) -> elem.Length = 5) (generatePopulation 100 5))
+
+    [<Fact>]  
+    member this.mutateCharacterWillDecrementOrIncrementTheCharacterAtRandom() =
+        let mutatedCharacters = List.init 50 (fun i -> mutateCharacter (char 89))
+        Assert.True([char 88; char 90] = List.ofSeq (Seq.distinct mutatedCharacters))
+
+    [<Fact>]  
+    member this.mutateCharacterWithCharacterHasMinimumValueIncrementsTheCharacter() =
+        Assert.Equal<char>(char 1, mutateCharacter (char 0))
+
+    [<Fact>]  
+    member this.mutateCharacterWithCharacterHasMaximumValueDecrementsTheCharacter() =
+        Assert.Equal<char>(char 126, mutateCharacter (char 127))
+
+    [<Fact>]  
+    member this.mutateCharacterWithCharacterIsNoASCIICharacterThrowsArgumentException() =
+        Assert.Throws<ArgumentException>(fun() -> mutateCharacter (char 128) |> ignore)
+
+    [<Fact>]  
+    member this.mutateCharacterAtIndexWithCharacterHasMinimumValueIncrementsTheCharacter() =
+        Assert.Equal<string>((char 1).ToString(), mutateCharacterAtIndex 0 ((char 0).ToString()))
+
+    [<Fact>]  
+    member this.mutateCharacterAtIndexWithCharacterHasMaximumValueDecrementsTheCharacter() =
+        Assert.Equal<string>((char 126).ToString(), mutateCharacterAtIndex 0 ((char 127).ToString()))
+
+    [<Fact>]  
+    member this.mutateCharacterAtIndexWithNullStringThrowsArgumentNullException() =
+        Assert.Throws<ArgumentNullException>(fun() -> mutateCharacterAtIndex 0 null |> ignore)
+
+    [<Fact>]  
+    member this.mutateCharacterAtIndexWithStringOfLengthZeroThrowsArgumentException() =
+        Assert.Throws<ArgumentException>(fun() -> mutateCharacterAtIndex 0 "" |> ignore)
+
+    [<Theory>]  
+    [<InlineData(-1)>]
+    [<InlineData(5)>]
+    [<InlineData(6)>]
+    member this.mutateCharacterAtIndexWithIndexIsInvalidThrowsArgumentException(index) =
+        Assert.Throws<ArgumentException>(fun() -> mutateCharacterAtIndex index "hello" |> ignore)
+
+    [<Fact>]  
+    member this.mutateWithProbabilityIsOneMutatesIndividual() =
+        Assert.NotEqual<string>("abcdefg", mutate 1.0 "abcdefg")
+
+    [<Fact>]  
+    member this.mutateWithProbabilityIsZeroDoesNotMutateIndividual() =
+        Assert.Equal<string>("abcdefg", mutate 0.0 "abcdefg")
+
+    [<Fact>]  
+    member this.mutateWithNullStringThrowsArgumentNullException() =
+        Assert.Throws<ArgumentNullException>(fun() -> mutate 0.5 null |> ignore)
+
+    [<Fact>]  
+    member this.mutateWithStringOfLengthZeroThrowsArgumentException() =
+        Assert.Throws<ArgumentException>(fun() -> mutate 0.5 "" |> ignore)
 
     [<Fact>]  
     member this.fitnessOnCorrectlyCalculatesFitness() =
@@ -25,12 +98,20 @@ type StringGeneticAlgorithmTests() =
 
     [<Fact>]  
     member this.fitnessOfMoreCorrectIndividualIsHigherThanLessCorrectIndividual() =
-        Assert.True(fitness "halla world" "hello world" > fitness "xalla porla" "hello world")
+        Assert.True(fitness "hello world" "halla world" > fitness "xalla porla" "hello world")
 
     [<Fact>]  
     member this.fitnessOnIndividualWithLengthLessThanTargetStringThrowsArgumentException() =
-        Assert.Throws<ArgumentException>(fun() -> fitness "hello worl" "hello world" |> ignore)
+        Assert.Throws<ArgumentException>(fun() -> fitness "hello world" "hello worl" |> ignore)
 
     [<Fact>]  
     member this.fitnessOnIndividualWithLengthGreaterThanTargetStringThrowsArgumentException() =
-        Assert.Throws<ArgumentException>(fun() -> fitness "hello worlds" "hello world" |> ignore)
+        Assert.Throws<ArgumentException>(fun() -> fitness "hello world" "hello worlds" |> ignore)
+
+    [<Fact>]  
+    member this.fitnessWithNullIndividualThrowsArgumentNullException() =
+        Assert.Throws<ArgumentNullException>(fun() -> fitness "hello world" null |> ignore)
+
+    [<Fact>]  
+    member this.fitnessWithNullTargetThrowsArgumentNullException() =
+        Assert.Throws<ArgumentNullException>(fun() -> fitness null "hello world" |> ignore)
