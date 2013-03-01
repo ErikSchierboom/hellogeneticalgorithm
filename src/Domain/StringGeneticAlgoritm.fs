6cb1e924c3198192be6302123bb265ac9a0e08c2
Erik Schierboom <erik_schierboom@hotmail.com>
2013-03-01 12:37:52 +0100

    Allow random individual to be generated

--- src/Domain/StringGeneticAlgoritm.fs
+++ src/Domain/StringGeneticAlgoritm.fs
@@ -2,14 +2,17 @@
 
 open System
 
-module HelloGeneticAlgorithm =
+module StringGeneticAlgorithm =
 
     let random = new Random()
+
+    let generateRandomIndividual length =
+        List.fold (fun acc i -> acc + Convert.ToChar(random.Next(128)).ToString()) "" [1..length]
 
     let numericCharacterValues (str:string) =
         List.map (fun c -> Convert.ToUInt16(c:char)) (List.ofArray (str.ToCharArray()))
 
-    let fitness (individual:string) =        
-        let zipped = List.zip (numericCharacterValues individual) (numericCharacterValues "hello world")
+    let fitness (individual:string) (target:string) =        
+        let zipped = List.zip (numericCharacterValues individual) (numericCharacterValues target)
         1.0 - List.fold (fun acc (x:uint16,y:uint16) -> acc + (Math.Abs(float x - float y) / float Char.MaxValue)) 0.0 zipped 
             
---
+++ tests/Domain.Tests/StringGeneticAlgoritmTests.fs
@@ -0,0 +1,36 @@
+﻿namespace Domain.Tests
+
+open Domain.StringGeneticAlgorithm
+open System
+open Xunit
+
+type StringGeneticAlgorithmTests() = 
+
+    [<Fact>]  
+    member this.generateRandomIndividualGeneratesRandomIndividualOfSpecifiedLength() =
+        let randomIndividual = generateRandomIndividual 10
+        Assert.True(randomIndividual.Length = 10)
+
+    [<Fact>]  
+    member this.generateRandomIndividualGeneratesRandomIndividuals() =      
+        Assert.False(generateRandomIndividual 10 = generateRandomIndividual 10)
+
+    [<Fact>]  
+    member this.numericCharacterValuesReturnsCorrectCharacterValues() =
+        Assert.True([uint16 104; uint16 101; uint16 108; uint16 108; uint16 111] = numericCharacterValues "hello")
+
+    [<Fact>]  
+    member this.fitnessOnCorrectlyCalculatesFitness() =
+        Assert.Equal(1.0, fitness "hello world" "hello world")
+
+    [<Fact>]  
+    member this.fitnessOfMoreCorrectIndividualIsHigherThanLessCorrectIndividual() =
+        Assert.True(fitness "halla world" "hello world" > fitness "xalla porla" "hello world")
+
+    [<Fact>]  
+    member this.fitnessOnIndividualWithLengthLessThanTargetStringThrowsArgumentException() =
+        Assert.Throws<ArgumentException>(fun() -> fitness "hello worl" "hello world" |> ignore)
+
+    [<Fact>]  
+    member this.fitnessOnIndividualWithLengthGreaterThanTargetStringThrowsArgumentException() =
+        Assert.Throws<ArgumentException>(fun() -> fitness "hello worlds" "hello world" |> ignore)
