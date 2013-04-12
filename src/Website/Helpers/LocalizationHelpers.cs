dd62c1238151e29988613ed3afeea5658cd62538
Erik Schierboom <erik_schierboom@hotmail.com>
2013-04-12 18:03:19 +0200

    Removed Glimpse and fixed validation globalization bug

---
+++ src/Website/Helpers/LocalizationHelpers.cs
@@ -0,0 +1,15 @@
+﻿namespace StudioDonder.HelloGeneticAlgorithm.Website.Helpers
+{
+    using System.Threading;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public static class LocalizationHelpers
+    {
+        public static IHtmlString MetaAcceptLanguage(this HtmlHelper html)
+        {
+            var acceptLanguage = HttpUtility.HtmlAttributeEncode(Thread.CurrentThread.CurrentUICulture.ToString());
+            return new HtmlString(string.Format("<meta name=\"accept-language\" content=\"{0}\">", acceptLanguage));
+        }
+    }
+}
