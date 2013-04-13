ae21585c015b81a81b7b8eace5e37efa40c445d8
Erik Schierboom <erik_schierboom@hotmail.com>
2013-04-13 21:03:51 +0200

    Updated README.md

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-hellogeneticalgorithm
-=====================
+# Hello Genetic Algorithm
+This project contains an implementation of a [generic algorithm](http://en.wikipedia.org/wiki/Genetic_algorithm) that allows text to be evolved from random strings using a very simple genetic algorithm. The implementation is done in F#, which is a functional language that is very well suited for this type of problems.
 
-A genetic algorithm that evolves to generate "Hello World"
+To make this project slightly more interesting, we have made the functionality available from a console application (not so hard) and from an ASP.NET MVC application. We have tried to use F# wherever we can use in the ASP.NET MVC application.
+
+## License
+[Apache License 2.0](https://github.com/studio-donder/framework-dotnet/blob/master/LICENSE.md)
--- src/Console/Program.fs
+++ src/Console/Program.fs
@@ -10,9 +10,9 @@ let printGeneration index generation =
     let (individual, fitness) = mostFitIndividual generation
     printfn "Generation %d: %s (%f)" (index + 1) individual fitness
 
-let printGenerations target size numberOfGenerations generations =
+let printGenerations target populationSize numberOfGenerations generations =
     printfn "Target: %s" target
-    printfn "Population size: %d" size
+    printfn "Population size: %d" populationSize
     printfn "Number of generation: %d" numberOfGenerations
     printfn ""
     List.iteri printGeneration generations
@@ -20,13 +20,13 @@ let printGenerations target size numberOfGenerations generations =
 [<EntryPoint>]
 let main argv = 
     let target = "erik"
-    let size = 20
+    let populationSize = 20
     let numberOfGenerations = 500
-    let generateMethod = generate target.Length size
+    let generateMethod = generate target.Length populationSize
     let crossoverMethod = onePoint 3
     let mutationMethod = mutate 0.05
     let fitnessMethod = fitness target       
     let selectionMethod = tournament 3
     let generations = simulate crossoverMethod mutationMethod fitnessMethod selectionMethod generateMethod numberOfGenerations
-    printGenerations target size numberOfGenerations generations
+    printGenerations target populationSize numberOfGenerations generations
     0 // return an integer exit code
