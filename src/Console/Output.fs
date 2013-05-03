c997923b90c91ff116f63234fd2ae6e22f1ec9fb
Erik Schierboom <erik_schierboom@hotmail.com>
2013-05-03 16:42:21 +0200

    Added input and output code for console

---
+++ src/Console/Output.fs
@@ -0,0 +1,17 @@
+﻿namespace StudioDonder.HelloGeneticAlgorithm.Console
+
+open System
+open StudioDonder.HelloGeneticAlgorithm.Domain.Fitness
+
+module Output =
+
+    let printGeneration index generation =
+        let (individual, fitness) = mostFitIndividual generation
+        printfn "Generation %d: %s (%f)" (index + 1) individual fitness
+
+    let printGenerations target populationSize numberOfGenerations generations =
+        printfn "Target: %s" target
+        printfn "Population size: %d" populationSize
+        printfn "Number of generation: %d" numberOfGenerations
+        printfn ""
+        List.iteri printGeneration generations
