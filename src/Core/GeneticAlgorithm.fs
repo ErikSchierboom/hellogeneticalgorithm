03e1d16d98d9a40939db91b0a8bc9e7c16b02b55
Erik Schierboom <erik_schierboom@hotmail.com>
2015-08-04 10:48:15 +0200

    Rewriting app

---
+++ src/Core/GeneticAlgorithm.fs
@@ -0,0 +1,20 @@
+﻿namespace GeneticAlgorithm
+
+type Gene = 
+    | Zero = 0 
+    | One = 1
+
+type Chromosome = {
+    Genes: Gene list
+    Fitness: float
+}
+
+module Genes =
+
+    let mutate gene = if gene = Gene.Zero then Gene.One else Gene.Zero
+
+module Chromosomes =
+
+    let map f x = { x with Genes = List.map f x.Genes }
+
+    let mutate shouldMutateGene mutateGene chromosome = map (fun x -> if shouldMutateGene x then mutateGene x else x) chromosome  
---
+++ tests/Core.Tests/GeneticAlgorithmTests.fs
@@ -0,0 +1,22 @@
+﻿namespace GeneticAlgorithm.Tests
+
+open GeneticAlgorithm
+open System
+open FsCheck
+open FsCheck.Xunit
+
+module GenesTests = 
+
+    [<Property>]
+    let ``Mutate returns flipped gene`` (gene: Gene) =
+        let mutated = Genes.mutate gene
+
+        if gene = Gene.Zero then mutated = Gene.One else mutated = Gene.Zero
+
+module ChromosomesTests =
+
+    [<Property>]
+    let ``Mutate with shouldMutateGene always true will flip all genes`` (chromosome: Chromosome) =       
+        let mutated = Chromosomes.mutate (fun _ -> true) Genes.mutate chromosome
+        
+        List.zip chromosome.Genes mutated.Genes |> List.forall (fun (x, y) -> not (x = y))
